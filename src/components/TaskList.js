4e389cde1824ed239956bc39416e051c235540a9
Alexandre Michetti Manduca <manduca@amazon.com>
2021-11-07 22:39:01 -0800

    Add InboxScreen

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -62,13 +62,10 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
 PureTaskList.propTypes = {
     /** Checks if it's in loading state */
     loading: PropTypes.bool,
-
     /** The list of tasks */
     tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
-
     /** Event to change the task to pinned */
     onPinTask: PropTypes.func,
-
     /** Event to change the task to archived */
     onArchiveTask: PropTypes.func,
 };
